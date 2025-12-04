bde5f66916b757ddec1bed44640aa70162fd7ac8
matsystems07 <matsystems07@gmail.com>
2025-12-04 22:35:18 +0500

    Update server.js

--- Fronted-Backend/server.js
+++ Fronted-Backend/server.js
@@ -35,7 +35,7 @@ const db = new Pool({
 
 // Login page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Login_Signup_Pages/login.html'));
+    res.sendFile(path.join(__dirname, 'Login_Signup_Pages/Login.html'));
 
 });
 
